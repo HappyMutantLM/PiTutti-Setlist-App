9cd2215210c696f8d26358807924028ba3ea2c81
leilamureebe <leila.mureebe@gmail.com>
2026-09-03 19:21:14 -0400

    Initial Commit

---
+++ PiTutti Setlist App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PiTutti Setlist App
+//
+//  Created by leila mureebe on 9/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PiTutti Setlist App/PiTutti_Setlist_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiTutti_Setlist_AppApp.swift
+//  PiTutti Setlist App
+//
+//  Created by leila mureebe on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PiTutti_Setlist_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
